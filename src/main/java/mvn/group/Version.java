28e8ce8dc6eb84ad9e78403d36be10627fc13f3b
Sukhdeep kaur <deep_sept20@yahoo.com>
2021-03-29 00:14:28 -0400

    maven project

---
+++ src/main/java/mvn/group/Version.java
@@ -0,0 +1,7 @@
+package mvn.group;
+
+public class Version {
+	/** Returns the version UID.
+	 */
+	public static final String UID = "1.0-SNAPSHOT";
+}
---
+++ src/main/java/mvn/group/mvn.group.java
@@ -0,0 +1,6 @@
+package mvn.group;
+
+public class mvn.group {
+	
+}
+
